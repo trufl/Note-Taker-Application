444e0696928e0e8435bc763ff3e9ee5e894e2138
T <endlesstruffle80@gmail.com>
2022-11-04 20:39:19 -0700

    save work

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 
-const PORT =  3001;    //process.env.PORT ||
+const PORT =  process.env.PORT || 3001; 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
